1ce0a04e114100fa245cff957620fd62ab97eded
CIRCULARKA <donkardon@list.ru>
2023-08-14 18:23:21 +0500

    Добавление логгирования событий для контроллера пользователей

--- Server/Challenger.Server/Controllers/UsersController.cs
+++ Server/Challenger.Server/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Challenger.Server.Controllers;
@@ -7,18 +8,27 @@ public class UsersController : ControllerBase
 	[HttpPost("/register")]
 	public string Register(User userToRegister)
 	{
+		Console.Write($"\"{nameof(Register)}\" method was called: ");
+		Console.WriteLine("Request method: " + HttpContext.Request.Method);
+
 		return "Registration";
 	}
 
 	[HttpPost("/auth")]
 	public string Authorize(string userLogin, string passwordHash)
 	{
+		Console.Write($"\"{nameof(Authorize)}\" method was called: ");
+		Console.WriteLine("Request method: " + HttpContext.Request.Method);
+
 		return "Authorization";
 	}
 
 	[HttpPost("/logout")]
 	public string Logout(string userLogin)
 	{
+		Console.Write($"\"{nameof(Logout)}\" method was called: ");
+		Console.WriteLine("Request method: " + HttpContext.Request.Method);
+
 		return "Logout";
 	}
 }
