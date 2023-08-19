9d69e80b312b44cc95e57b86beeb3c613c5a3c89
CIRCULARKA <donkardon@list.ru>
2023-08-19 09:30:41 +0500

    Переход от minimal api к стандарту

--- Server/Challenger.Server/Startup.cs
+++ Server/Challenger.Server/Startup.cs
@@ -1,10 +1,24 @@
+using System;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+
 namespace Challenger.Server;
 
 public class Startup
 {
+	public IConfiguration Configuration { get; init; }
+
+	public Startup(IConfiguration config)
+	{
+		Configuration = config;
+	}
+
 	public void ConfigureServices(IServiceCollection services)
 	{
 		services.AddControllers();
+		Console.WriteLine("Connection string: " + Configuration["ConnectionString"]);
 	}
 
 	public void Configure(IApplicationBuilder app)
