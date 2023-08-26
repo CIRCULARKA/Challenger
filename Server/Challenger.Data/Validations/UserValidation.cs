54ed85099be7d700036911c9f1a53672ac380275
CIRCULARKA <donkardon@list.ru>
2023-08-26 16:50:16 +0500

    Добавление сообщения о неверном email

--- Server/Challenger.Data/Validations/UserValidation.cs
+++ Server/Challenger.Data/Validations/UserValidation.cs
@@ -11,6 +11,6 @@ public class UserValidator : AbstractValidator<User>
 		RuleFor(u => u.Login).Length(3, 255).WithMessage("Login can't be larger than 255 symbols and shorter than 3 symbols");
 		RuleFor(u => u.Age).InclusiveBetween<User, short>(0, 120).WithMessage("Your age should be real");
 		RuleFor(u => u.Weight).InclusiveBetween<User, short>(0, 500).WithMessage("Your weight should be real");
-		RuleFor(u => u.Email).EmailAddress();
+		RuleFor(u => u.Email).EmailAddress().WithMessage("Specify valid email address");
 	}
 }
