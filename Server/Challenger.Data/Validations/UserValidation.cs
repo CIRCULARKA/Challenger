7a9ec3dca3326e75d629f7961ffa1a6cc59586d6
CIRCULARKA <donkardon@list.ru>
2023-08-26 16:17:13 +0500

    Создание валидатора для модели пользователя

---
+++ Server/Challenger.Data/Validations/UserValidation.cs
@@ -0,0 +1,16 @@
+using FluentValidation;
+
+using Challenger.Data.Models;
+
+namespace Challenger.Data.Validation;
+
+public class UserValidator : AbstractValidator<User>
+{
+	public UserValidator()
+	{
+		RuleFor(u => u.Login).Length(3, 255).WithMessage("Login can't be larger than 255 symbols and shorter than 3 symbols");
+		RuleFor(u => u.Age).InclusiveBetween<User, short>(0, 120).WithMessage("Your age should be real");
+		RuleFor(u => u.Weight).InclusiveBetween<User, short>(0, 500).WithMessage("Your weight should be real");
+		RuleFor(u => u.Email).Matches("\\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\\b").WithMessage("Incorrect email address");
+	}
+}
